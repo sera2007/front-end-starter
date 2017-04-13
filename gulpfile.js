eb00cccc9ad23623c399c97bf630854df61fd528
Sergio <sera.serduk@gmail.com>
2017-04-13 11:15:37 +0300

    Fixed:  - scss error  - pug BrowserSync
     Added:
     - gulp-plumber for pug debugging errors withot stop gulping process

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ var cache = require('gulp-cache');
 var del = require('del');
 var runSequence = require('run-sequence');
 var pug = require('gulp-pug');
+var plumber = require('gulp-plumber');
 
 // Development Tasks
 // -----------------
@@ -29,22 +30,22 @@ gulp.task('browserSync', function() {
   })
 })
 
-var pug = require('gulp-pug');
-
+// Pug compiling
 gulp.task('pug', function buildHTML() {
     return gulp.src('app/pug/*.pug')
+        .pipe(plumber())
         .pipe(pug({
             // Your options in here.
         }))
         .pipe(gulp.dest('app/'));
 });
-
-gulp.task('templates-watch', ['pug'], function (done) {
+// Pug additional watcher
+gulp.task('pug-watch', ['pug'], function (done) {
     browserSync.reload();
+    //console.log('pug reload');
     done();
 });
-
-
+// SASS compiling
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
     .pipe(sourcemaps.init())
@@ -55,16 +56,16 @@ gulp.task('sass', function() {
     .pipe(browserSync.reload({ // Reloading with Browser Sync
       stream: true
     }));
-})
+});
 
 // Watchers
 gulp.task('watch', function() {
   gulp.watch('app/scss/**/*.scss', ['sass']);
-  gulp.watch('app/pug/*.pug', ['templates-watch']);
+  gulp.watch('app/pug/*.pug', ['pug']);
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/css/print.css', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
-})
+});
 
 // Optimization Tasks 
 // ------------------
@@ -93,35 +94,35 @@ gulp.task('images', function() {
 gulp.task('fonts', function() {
   return gulp.src('app/fonts/**/*')
     .pipe(gulp.dest('dist/fonts'))
-})
+});
 
 // Copying js libs
 gulp.task('copy-js-libs', function() {
     return gulp.src('app/js/libs/**/*')
         .pipe(gulp.dest('dist/js/libs'))
-})
+});
 // Copying favicon
 gulp.task('copy-favicon', function() {
     return gulp.src('app/favicon.ico')
         .pipe(gulp.dest('dist'))
-})
+});
 // Copying print
 gulp.task('copy-css-files', function() {
     return gulp.src('app/css/print.css')
         .pipe(gulp.dest('dist/css'))
-})
+});
 // Copying style
 gulp.task('copy-css-files2', function() {
     return gulp.src('app/css/style.css')
         .pipe(gulp.dest('dist/css'))
-})
+});
 
 // Cleaning 
 gulp.task('clean', function() {
   return del.sync('dist').then(function(cb) {
     return cache.clearAll(cb);
   });
-})
+});
 
 gulp.task('clean:dist', function() {
   return del.sync(['dist/**/*', '!dist/img', '!dist/img/**/*']);
@@ -131,10 +132,10 @@ gulp.task('clean:dist', function() {
 // ---------------
 
 gulp.task('default', function(callback) {
-  runSequence(['sass', 'browserSync'], 'watch',
+  runSequence(['pug','sass','browserSync'], 'watch',
     callback
   )
-})
+});
 
 gulp.task('build', function(callback) {
   runSequence(
@@ -143,4 +144,4 @@ gulp.task('build', function(callback) {
     ['useref', 'images', 'fonts','copy-favicon','copy-css-files','copy-css-files2'],
     callback
   )
-})
+});
