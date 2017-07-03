5f56aeca5f9bf2225f388d81d30f024d77714485
Sergio <sera.serduk@gmail.com>
2017-07-03 11:36:35 +0300

    - animate.css  - hover.css  - wow.js  - elements.html integrated instead of some elements inside the index.html  - jquery version updated  - hamburger menu sample integrated  - mobile menu behavior integrated  - font-awesome integrated

--- gulpfile.js
+++ gulpfile.js
@@ -73,7 +73,7 @@ gulp.task('useref', function() {
   return gulp.src('app/*.html')
     .pipe(useref())
     .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulpIf('*.css', cssnano({autoprefixer: autoprefixerOptions})))
     .pipe(gulp.dest('dist'));
 });
 
